c1bdf11dc08f46b7b122033822733fe693f7004e
KarenF <karenf@outlook.com>
2018-03-22 17:23:20 -0300

    refactor search

--- src/album.js
+++ src/album.js
@@ -2,13 +2,10 @@ import { API_URL } from './config'
 import { toJSON } from './utils'
 
 export const getAlbum = id =>
-  fetch(`${API_URL}/albums/${id}`)
-    .then(toJSON)
+  fetch(`${API_URL}/albums/${id}`).then(toJSON)
 
 export const getAlbums = id =>
-  fetch(`${API_URL}/albums/?ids=${id}`)
-    .then(toJSON)
+  fetch(`${API_URL}/albums/?ids=${id}`).then(toJSON)
 
 export const getAlbumTracks = id =>
-  fetch(`${API_URL}/albums/${id}/tracks`)
-    .then(toJSON)
+  fetch(`${API_URL}/albums/${id}/tracks`).then(toJSON)
--- tests/search.spec.js
+++ tests/search.spec.js
@@ -10,7 +10,7 @@ sinonStubPromise(sinon)
 
 global.fetch = require('node-fetch')
 
-describe('Spotify Wrapper', () => {
+describe('Search', () => {
   let fetchedStub
   let promise
 
