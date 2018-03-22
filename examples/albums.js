a69946bc2bf55aec4c455e0125f356f87505a8ba
KarenF <karenf@outlook.com>
2018-03-22 00:49:36 -0300

    add getAlbum

--- examples/albums.js
+++ examples/albums.js
@@ -5,3 +5,4 @@ global.fetch = require('node-fetch')
 const albums = searchAlbums('Incubus')
 
 albums.then(data => console.log(data))
+//albums.then(data => data.albums.item.map(item => console.log(item.name))
---
+++ tests/album.spec.js
@@ -0,0 +1,58 @@
+import chai, { expect } from 'chai'
+import sinon from 'sinon'
+import sinonChai from 'sinon-chai'
+import sinonStubPromise from 'sinon-stub-promise'
+
+import { getAlbum, getAlbumTracks } from '../src/album'
+
+chai.use(sinonChai)
+sinonStubPromise(sinon)
+
+global.fetch = require('node-fetch')
+
+describe('Album', () => {
+  let fetchedStub
+  let promise
+
+  beforeEach(() => {
+    fetchedStub = sinon.stub(global, 'fetch')
+    promise = fetchedStub.returnsPromise()
+  })
+
+  afterEach(() => {
+    fetchedStub.restore()
+  })
+
+  describe('smoke tests', () => {
+    it('should have getAlbum method', () => {
+      expect(getAlbum).to.exist
+    })
+
+    it('should have getAlbumTracks method', () => {
+      expect(getAlbumTracks).to.exist
+    })
+  })
+
+  describe('getAlbum', () => {
+    it('should call fetch method', () => {
+      const album = getAlbum()
+      expect(fetchedStub).to.have.been.calledOnce
+    })
+
+    it('should call fetch with the correct URL', () => {
+      const album = getAlbum('4aawyAB9vmqN3uQ7FjRGTy')
+      expect(fetchedStub).to.have.been
+        .calledWith('https://api.spotify.com/v1/albums/4aawyAB9vmqN3uQ7FjRGTy')
+
+      const album2 = getAlbum('4aawyAB9vmqN3uQ7FjRGTk')
+      expect(fetchedStub).to.have.been
+        .calledWith('https://api.spotify.com/v1/albums/4aawyAB9vmqN3uQ7FjRGTk')
+    })
+
+    it('should return the correct data from Promise', () => {
+      promise.resolves({ album: 'name' })
+      const album = getAlbum('4aawyAB9vmqN3uQ7FjRGTy')
+      expect(album.resolveValue).to.be.eql({ album: 'name' })
+    })
+  })
+})
