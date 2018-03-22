829976f4cbb1319b3922ab813fb94f15f9713e9a
KarenF <karenf@outlook.com>
2018-03-22 17:42:16 -0300

    refactor

--- src/album.js
+++ src/album.js
@@ -2,10 +2,13 @@ import { API_URL } from './config'
 import { toJSON } from './utils'
 
 export const getAlbum = id =>
-  fetch(`${API_URL}/albums/${id}`).then(toJSON)
+  // fetch(`${API_URL}/albums/${id}`).then(toJSON)
+  fetch(`https://api.spotify.com/v1/albums/${id}`).then(toJSON)
 
 export const getAlbums = id =>
-  fetch(`${API_URL}/albums/?ids=${id}`).then(toJSON)
+  // fetch(`${API_URL}/albums/?ids=${id}`).then(toJSON)
+  fetch(`https://api.spotify.com/v1/albums/?ids=${id}`).then(toJSON)
 
 export const getAlbumTracks = id =>
-  fetch(`${API_URL}/albums/${id}/tracks`).then(toJSON)
+  // fetch(`${API_URL}/albums/${id}/tracks`).then(toJSON)
+  fetch(`https://api.spotify.com/v1/albums/${id}/tracks`).then(toJSON)
--- src/search.js
+++ src/search.js
@@ -2,7 +2,8 @@ import { API_URL } from './config'
 import { toJSON } from './utils'
 
 export const search = (query, type) =>
-  fetch(`${API_URL}/search?q=${query}&type=${type}`).then(toJSON)
+  // fetch(`${API_URL}/search?q=${query}&type=${type}`).then(toJSON)
+  fetch(`https://api.spotify.com/v1/search?q=${query}&type=${type}`).then(toJSON)
 
 export const searchArtists = query =>
   search(query, 'artist')
