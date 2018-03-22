c3748d5bce429d63c30ea4d6f9acb748f13f21f5
KarenF <karenf@outlook.com>
2018-03-22 17:32:01 -0300

    refactor

---
+++ src/index.js
@@ -0,0 +1,22 @@
+import {
+  search,
+  searchAlbums,
+  searchArtists,
+  searchPlaylists
+} from './search'
+
+import {
+  getAlbum,
+  getAlbums,
+  getAlbumTracks
+} from './album'
+
+module.exports = {
+  search,
+  searchAlbums,
+  searchArtists,
+  searchPlaylists,
+  getAlbum,
+  getAlbums,
+  getAlbumTracks
+}
