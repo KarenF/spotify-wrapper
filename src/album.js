37e15d9ab13ddb9f922f675485fd8f0cdee9d139
KarenF <karenf@outlook.com>
2018-03-22 16:43:00 -0300

    add getAlbums

--- src/album.js
+++ src/album.js
@@ -2,4 +2,7 @@ export const getAlbum = id =>
   fetch(`https://api.spotify.com/v1/albums/${id}`)
     .then(data => data.json())
 
+export const getAlbums = id =>
+  fetch(`https://api.spotify.com/v1/albums/?ids=${id}`)
+
 export const getAlbumTracks = () => {}
--- tests/album.spec.js
+++ tests/album.spec.js
@@ -3,7 +3,7 @@ import sinon from 'sinon'
 import sinonChai from 'sinon-chai'
 import sinonStubPromise from 'sinon-stub-promise'
 
-import { getAlbum, getAlbumTracks } from '../src/album'
+import { getAlbum, getAlbums, getAlbumTracks } from '../src/album'
 
 chai.use(sinonChai)
 sinonStubPromise(sinon)
@@ -54,5 +54,28 @@ describe('Album', () => {
       const album = getAlbum('4aawyAB9vmqN3uQ7FjRGTy')
       expect(album.resolveValue).to.be.eql({ album: 'name' })
     })
+  })
+
+  describe('getAlbums', () => {
+    it('should call fetch method', () => {
+      const albums = getAlbums()
+      expect(fetchedStub).to.have.been.calledOnce
+    })
+
+    it('should call fetch with the correct URL', () => {
+      const albums = getAlbums(['4aawyAB9vmqN3uQ7FjRGTy', '4aawyAB9vmqN3uQ7FjRGTk'])
+      expect(fetchedStub).to.have.been
+        .calledWith('https://api.spotify.com/v1/albums/?ids=4aawyAB9vmqN3uQ7FjRGTy,4aawyAB9vmqN3uQ7FjRGTk')
+    })
+
+    it('should return the correct data from Promise', () => {
+      promise.resolves({ album: 'name' })
+      const albums = getAlbums(['4aawyAB9vmqN3uQ7FjRGTy', '4aawyAB9vmqN3uQ7FjRGTk'])
+      expect(albums.resolveValue).to.be.eql({ album: 'name' })
+    })
+  })
+
+  describe('getAlbumTracks', () => {
+    it('should call fetch method')
   })
 })
